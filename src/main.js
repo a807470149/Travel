0a9a991cf7b5b9aaa8247ed64e818810d8aa7280
unknown <807470149@qq.com>
2018-10-08 20:41:45 +0800

    'change'

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 // 300毫秒点击延迟解决方案
 import fastClick from 'fastclick'
-
+// 轮播图插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 // 统一不同浏览器的默认样式
 import './assets/styles/reset.css'
-
 // 1像素边框解决方案
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
   render: h => h(App)
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data () {
+        return{
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                // 让图片支持循环轮播
+                loop: true
+            },
+            swiperList: [{
+                id: '001',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/2a/c58ac6dcadd7d502.jpg_750x200_660cb194.jpg'
+            },{
+                id: '002',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/c6/2467595fffc3b302.jpg_750x200_cca13d51.jpg'
+            },{
+                id: '003',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+            },{
+                id: '004',
+                imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/eb/5cacf42b59e91e02.jpg_750x200_9c0a77b0.jpg'
+            }]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+        
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 25%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
+

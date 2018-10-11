8a32a5c3acd00fd60252609cceb8fa77eea1af21
unknown <807470149@qq.com>
2018-10-11 22:05:20 +0800

    finish city page

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -14,6 +14,7 @@ import HomeIcons from "./components/Icons";
 import HomeRecommend from "./components/Recommend";
 import HomeWeekend from "./components/Weekend";
 import axios from "axios";
+import { mapState } from 'vuex'
 
 export default {
   name: "Home",
@@ -29,12 +30,16 @@ export default {
         swiperList: [],
         iconList: [],
         recommendList: [],
-        weekendList: []
+        weekendList: [],
+        lastCity: ''
     };
+  },
+  computed: {
+    ...mapState(['city'])
   },
   methods: {
     getHomeInfo() {
-      axios.get("/mock/index.json").then(this.getHomeInfoSucc);
+      axios.get("/mock/index.json?city=" + this.city).then(this.getHomeInfoSucc);
     },
     getHomeInfoSucc(res) {
         res = res.data
@@ -48,7 +53,14 @@ export default {
     }
   },
   mounted() {
+    this.lastCity = this.city
     this.getHomeInfo();
+  },
+  activated () {  // 当页面被重新加载时触发
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 };
 </script>

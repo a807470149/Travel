f37d09a2b4066f9ab245b6d0e45eb34a1d8e3231
unknown <807470149@qq.com>
2018-10-11 13:29:59 +0800

    city components finish

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item, index) in cities" :key="index">
+            <div class="area" v-for="(item, index) in cities" :key="index" :ref="index">
                 <div class="title border-topbottom">{{ index }}</div>
                 <div class="item-list" v-for="innerItem in item" :key="innerItem.id">
                     <div class="item border-bottom">{{ innerItem.name }}</div>
@@ -33,11 +33,20 @@ export default {
     name: 'CityList',
     props: {
         cities: Object,
-        hotCities: Array
+				hotCities: Array,
+				letter: String
     },
     mounted () {
         this.scroll = new Bscroll(this.$refs.wrapper)
-    }
+		},
+		watch: {
+			letter () {
+				if (this.letter) {
+					const element = this.$refs[this.letter][0]
+					this.scroll.scrollToElement(element)
+				}
+			}
+		}
 }
 </script>
 
